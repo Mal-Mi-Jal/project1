d6c889e6bdd55b87091be4f2e2de8d960470bd40
Mal-Mi-Jal <gom15465@gmail.com>
2026-04-08 13:52:25 +0900

    프로젝트 초기화 & 깃허브 연결

---
+++ src/main/java/com/github/scproject1/ScProject1Application.java
@@ -0,0 +1,13 @@
+package com.github.scproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScProject1Application.class, args);
+    }
+
+}
